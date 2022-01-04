f959c8a1078dd3fede4819b8c39aee1e312b9c7d
Domenik Jones <domenik.jones@gmail.com>
2022-01-04 14:37:38 +0100

    clean gui.py and fix some errors

--- gui.py
+++ gui.py
@@ -4,59 +4,65 @@ import os
 import argparse
 
 from time import sleep
-from guizero import App, Text, TextBox, MenuBar, info
+from guizero import App, Text, TextBox, MenuBar
 
-### Setting initial values for color variables ###
-bgvalue = ""
-txvalue = ''
-posvalue = ''
-txtbox_pos = "#38BC38"
-txtbox_neg = "#BC3838"
-### Menu bar for display options###
-        
+parser = argparse.ArgumentParser(description="PyCryptoBot Telegram Bot")
+parser.add_argument(
+    "--config",
+    type=str,
+    dest="config_file",
+    help="pycryptobot config file",
+    default="config.json",
+)
+parser.add_argument(
+    "--datafolder",
+    type=str,
+    help="Use the datafolder at the given location, useful for multi bots running in different folders",
+    default="",
+)
+args = parser.parse_args()
+
+# Setting initial values for color variables
+# bg_value = ""
+# txvalue = ''
+# posvalue = ''
+# txtbox_pos = "#38BC38"
+# txtbox_neg = "#BC3838"
+# Menu bar for display options
+
+
 def about():
-        app.info("info",        "Pycryptobot GUI by              Mark H. & Jared S.")
+    app.info("info", "Pycryptobot GUI by              Mark H. & Jared S.")
+
+
 def main_bg():
-        bgvalue = app.question("Background", "Enter Hex Color Value.")
-        if bgvalue is not None:
-            app.bg = bgvalue
-        
+    bg_value = app.question("Background", "Enter Hex Color Value.")
+    if bg_value is not None:
+        app.bg = bg_value
+
+
 def text_clr():
-        txvalue = app.question("Text", "Enter Hex Color Value.")
-        if txvalue is not None:
-            app.text_color = txvalue
-### this feature not working at this time
-##def pos_clr():
-##        posvalue = app.question("Text", "Enter Hex Color Value.")
-##        if txvalue is not None:
-##            txtbox.bg = posvalue
-##        print("Positive Color")
-##
-##def neg_clr():
-##        print("Negative Color")
-##
-### updates via telegram_data/data.json for the selected trading pair,
+    text_value = app.question("Text", "Enter Hex Color Value.")
+    if text_value is not None:
+        app.text_color = text_value
+
+
+# this feature not working at this time
+# def pos_clr():
+#        posvalue = app.question("Text", "Enter Hex Color Value.")
+#        if txvalue is not None:
+#            txtbox.bg = posvalue
+#        print("Positive Color")
+#
+# def neg_clr():
+#        print("Negative Color")
+#
+# updates via telegram_data/data.json for the selected trading pair,
+
 
 class GuiSetup:
     def __init__(self) -> None:
         self.datafolder = ""
-
-        parser = argparse.ArgumentParser(description="PyCryptoBot Telegram Bot")
-        parser.add_argument(
-            "--config",
-            type=str,
-            dest="config_file",
-            help="pycryptobot config file",
-            default="config.json",
-        )
-        parser.add_argument(
-            "--datafolder",
-            type=str,
-            help="Use the datafolder at the given location, useful for multi bots running in different folders",
-            default="",
-        )
-
-        args = parser.parse_args()
 
         self.config_file = args.config_file
 
@@ -68,8 +74,9 @@ class GuiSetup:
 
         if not args.datafolder == "":
             self.datafolder = args.datafolder
-### json reading  ###
+
     def read_file(self, file):
+        """Read telegram JSON config file"""
         read = False
         while read is False:
             try:
@@ -84,15 +91,15 @@ class GuiSetup:
                 read = False
 
         return data
-### Reads data.json and adds the correct amount of display boxes ###
-    def add_pairs(self):
 
+    def add_pairs(self):
+        """Reads data.json and adds the correct amount of display boxes"""
         jsonfiles = os.listdir(os.path.join(self.datafolder, "telegram_data"))
         while len(jsonfiles) <= 1:
             sleep(300)
         row = 2
         for file in jsonfiles:
-            if not "data.json" in file:
+            if "data.json" not in file:
                 data = self.read_file(file)
 
                 tpair = TextBox(app, grid=[0, row], width=15, align="top")
@@ -121,55 +128,57 @@ class GuiSetup:
                 OBV.repeat(5100, self.update_indicator, args=[OBV, file, "EMA"])
 
                 row += 1
-### updates the margin and delta boxes ###
 
     def update_textbox(self, txtbox, file, key) -> None:
+        """Updates the margin and delta boxes"""
         data = self.read_file(file)
-
-        last_price = txtbox.value if txtbox.value != "" else data[key]
-
-        txtbox.value = data[key] if not data[key] == " " else "N/A"
-### this line causes the price tickers to flash instead of stay a constant state   ###
-### originally added to turn the margin and delta boxes back to an inactive state after a sale ###
+        value = data.get(key, 0)
+        last_price = txtbox.value if txtbox.value != "" else value
+        txtbox.value = value if not value == " " else "N/A"
+        # this line causes the price tickers to flash instead of stay a constant state
+        # originally added to turn the margin and delta boxes back to an inactive state after a sale
         txtbox.bg = app.bg
-        
         if key in ("margin", "delta") and txtbox.value != "N/A":
             txtbox.bg = (
                 "#38BC38" if float(txtbox.value.replace("%", "")) > 0 else "#BC3838"
             )
-           
-### Tickers change state with price change ###
+        # Tickers change state with price change
         if key == "price":
             if float(last_price) < float(txtbox.value):
                 txtbox.bg = "#38BC38"
             elif float(last_price) > float(txtbox.value):
                 txtbox.bg = "#BC3838"
-### Changes indicator background states ###
+
     def update_indicator(self, txtbox, file, key) -> None:
+        """Changes indicator background states"""
         data = self.read_file(file)
-        txtbox.value = data["indicators"][key] if key in data["indicators"] else False
+        indicators = data.get('indicators', {})
+        txtbox.value = indicators.get(key) if key in indicators.keys() else False
         txtbox.bg = "#38BC38" if txtbox.value == "True" else "#BC3838"
-### Changes last action text and background states ###
+
     def update_lastaction(self, txtbox, file) -> None:
+        """Changes last action text and background states"""
         data = self.read_file(file)
-        txtbox.value = "BUY" if not data["margin"] == " " else "SELL"
-        txtbox.bg =  "#38BC38" if not data["margin"] == " " else app.bg
+        margin = data.get('margin')
+        txtbox.value = "BUY" if not margin == " " else "SELL"
+        txtbox.bg = "#38BC38" if not margin == " " else app.bg
         
 
-### Size, Color, and Title can be modified here ###
-
+# Size, Color, and Title can be modified here
 app = App(layout="grid", height=730, width=1320)
 app.title = "Pycryptobot SimpleGUI V0.2"
 app.bg = "black"
 app.text_color = "white"
 
-### menu bar ###
+# Menu bar
 menu = MenuBar(app,
                toplevel=["Display Options"],
-               options=[
-                   [["Background Color", main_bg],["Text Color", text_clr], ["About", about]]
-                   ])
-###labels for the top of the display###
+               options=[[
+                   ["Background Color", main_bg],
+                   ["Text Color", text_clr],
+                   ["About", about]]])
+
+# Labels for the top of the display###
 tpair_label = Text(app, text=" Trading Pair", grid=[0, 1])
 exchange_label = Text(app, text="Exchange", grid=[1, 1])
 action_label = Text(app, text="Last Action", grid=[2, 1])
@@ -182,8 +191,6 @@ EMA_label = Text(app, text="EMA", grid=[8, 1])
 MACD_label = Text(app, text="MACD", grid=[9, 1])
 OBV_label = Text(app, text="OBV", grid=[10, 1])
 
-###making boxes for the display###
-
+# Making boxes for the display
 GuiSetup().add_pairs()
-
 app.display()
